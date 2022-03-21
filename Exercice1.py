33f3bd5f30d88ab705cc1f68278e8ce633ef9318
SofKh <sofiane.chkh@gmail.com>
2022-03-21 10:21:58 -0400

    done

--- Exercice1.py
+++ Exercice1.py
@@ -1,4 +1,20 @@
 # Exercice 1:
 # Créer une liste contenant tous les nombres premiers entre 2 et 20. Ensuite, demander à l'utilisateur d'écrire un nombre
-# entre 2 et 20 (bien vérifier si c'est le cas) et vérifier si ce nombre est prime à l'aide de votre liste
+# entre 2 et 20 (bien vérifier si c'est le cas) et vérifier si ce nombre est premier à l'aide de votre liste
 # en affichant le résultat à la console.
+
+def prime():
+    premiers = [2, 3, 5, 7, 11, 13, 17, 19]
+
+    utilisateur = int(input("Merci d'écrire un nombre entre 2 et 20: "))
+    if 2 <= utilisateur <= 20:
+        print(utilisateur in premiers)
+    else:
+        return "Votre nombre n'est pas entre 2 et 20"
+
+prime()
+
+
+
+
+
