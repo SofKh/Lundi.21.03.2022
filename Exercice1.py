98054a41c01555a08042efd396198bf6b6dd1b7e
SofKh <sofiane.chkh@gmail.com>
2022-03-21 09:35:58 -0400

    modifs

---
+++ Exercice1.py
@@ -0,0 +1,4 @@
+# Exercice 1:
+# Créer une liste contenant tous les nombres premiers entre 2 et 20. Ensuite, demander à l'utilisateur d'écrire un nombre
+# entre 2 et 20 (bien vérifier si c'est le cas) et vérifier si ce nombre est prime à l'aide de votre liste
+# en affichant le résultat à la console.
